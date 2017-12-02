e51a276d5bcbe4ebd5b6259899fb09d56f7c789a
chiew <chiewminhai@gmail.com>
2017-12-02 23:17:22 +0800

    Initial commit

---
+++ myblog/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
+import markdown
+from django.utils.html import strip_tags
+from django.utils.six import python_2_unicode_compatible
+# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+class Post(models.Model):
+    title = models.CharField(max_length=70)
+    body = models.TextField()
+    create_time = models.DateTimeField()
+    modified_time = models.DateTimeField()
+    excerpt = models.CharField(max_length=200, blank=True)
+    category = models.ForeignKey(Category)
+    tags = models.ManyToManyField(Tag, blank=True)
+    author = models.ForeignKey(User)
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk})
+
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            self.excerpt = strip_tags(md.convert(self.body)[:54])
+
+        super(Post, self).save(*args, **kwargs)
